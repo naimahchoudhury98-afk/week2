76e1d223915df3a106eaf706fd1cf2828547a8bd
Naimah Choudhury <naimah.choudhury98@gmail.com>
2026-01-16 10:02:05 +0000

    basiccss

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
-console.log("Hello, testing")
-const thumbnailsContainer = document.querySelector('.thumbnails')
+console.log("Hello, testing");
+
+const thumbnailsContainer = document.querySelector("#thumbnails");
 
 const images = [
-   { src: "./Images/modern minimal bedroom.jpg",
-    alt: "Bedroom with minimal furnishing and modern interior"
-},
-   { src:"./Images/white orange bedroom.jpg",
-    alt: "Bedroom with white and orange interior"
-}, 
-   { src: "./Images/grey bedroom.jpg",
-    alt: "Bedroom with grey interior"
-},
-   { src: "./Images/beige blue bedroom.jpg",
-    alt: "Bedroom with blue interior"
-}    
-]
+  {
+    src: "./Images/modern minimal bedroom.jpg",
+    alt: "Bedroom with minimal furnishing and modern interior",
+  },
+  {
+    src: "./Images/white orange bedroom.jpg",
+    alt: "Bedroom with white and orange interior",
+  },
+  {
+    src: "./Images/grey bedroom.jpg",
+    alt: "Bedroom with grey interior",
+  },
+  {
+    src: "./Images/beige blue bedroom.jpg",
+    alt: "Bedroom with blue interior",
+  },
+];
 
-//create a function
+function createThumbnails() {
+  for (let i = 0; i < images.length; i++) {
+    const imgElement = document.createElement("img");
 
-function createThumbnail (){
-    images.foreach(function (image){
-        const imageElement = document.createElement('img')
-    })
+    imgElement.src = images[i].src;
+    imgElement.alt = images[i].alt;
+
+    imgElement.addEventListener("click", function () {
+      console.log(images[i]);
+      // big image will go here next
+    });
+
+    thumbnailsContainer.append(imgElement);
+  }
 }
 
-//create, set it, append
+createThumbnails();
