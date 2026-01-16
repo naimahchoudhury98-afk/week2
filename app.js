f97874e607457fc035154a7f4bf5c8509768d483
Naimah Choudhury <naimah.choudhury98@gmail.com>
2026-01-16 11:16:20 +0000

    addingleftrightkeys

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 console.log("Hello, testing");
 
 const thumbnailsContainer = document.querySelector("#thumbnails");
+const bigDisplay = document.querySelector("#bigDisplay");
+
+const prevBtn = document.querySelector("#Prev");
+const nextBtn = document.querySelector("#Nxt");
 
 const images = [
   {
@@ -21,17 +25,32 @@ const images = [
   },
 ];
 
+let currentIndex = 0;
+
+
+function createBigImage(imageData) {
+  bigDisplay.innerHTML = "";
+
+  const bigImage = document.createElement("img");
+  bigImage.src = imageData.src;
+  bigImage.alt = imageData.alt;
+
+  bigDisplay.append(bigImage);
+}
+
+function showImageByIndex(index) {
+  currentIndex = index;
+  createBigImage(images[currentIndex]);
+}
+
 function createThumbnails() {
   for (let i = 0; i < images.length; i++) {
     const imgElement = document.createElement("img");
-
     imgElement.src = images[i].src;
     imgElement.alt = images[i].alt;
 
     imgElement.addEventListener("click", function () {
-      console.log(images[i]);
-      createBigImage(images[i])
-      
+      showImageByIndex(i);
     });
 
     thumbnailsContainer.append(imgElement);
@@ -39,12 +58,25 @@ function createThumbnails() {
 }
 createThumbnails();
 
-function createBigImage(imageData){
-    bigDisplay.innerHTML= ""
-    const bigImage= document.createElement("img")
 
-    bigImage.src= imageData.src
-    bigImage.alt= imageData.alt
+prevBtn.addEventListener("click", function () {
+  let newIndex = currentIndex - 1;
 
-    bigDisplay.append(bigImage)
-}
+  if (newIndex < 0) {
+    newIndex = images.length - 1;
+  }
+
+  showImageByIndex(newIndex);
+});
+
+nextBtn.addEventListener("click", function () {
+  let newIndex = currentIndex + 1;
+
+  if (newIndex >= images.length) {
+    newIndex = 0;
+  }
+    showImageByIndex(newIndex);
+});
+
+
+showImageByIndex(0);
